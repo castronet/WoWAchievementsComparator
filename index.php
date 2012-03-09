1535853e6fcb39cf838a5c56f19ac35510431db0
hex <hex@castronet.org>
2012-03-09 12:53:04 +0100

    Added index file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+# vi:si:et:sw=4:sts=4:ts=4
+?>
+<html>
+    <head>
+        <title>Achievements Comparator</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="Author" content="^Hex^"/>
+        <meta name="Generator" content="Vim"/>
+<?php /*<link rel="shortcut icon" href="/dibs/uec-ico.png" type="image/gif"/>*/ ?>
+    </head>
+
+    <body>
+    </body>
+</html>
